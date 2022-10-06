764b44442a2178d109d63d228b16fee60658c39a
mo_shark <medoelzarkalt@gmail.com>
2022-10-06 06:33:56 -0400

    momo be scripting

---
+++ kubejs/server_scripts/momo_scripts.js
@@ -0,0 +1,18 @@
+onEvent('recipes', event => {
+// fyi i dont think you need the recipes event because the function already asks for it
+// but i havent tested in-game to check
+// the function altarRecipe is found in functions.js (you can also ctrl + click on the function)
+    altarRecipe('bloodmagic:blankslate', 'botania:quartz_elven', 0, 1000, 10, 2);
+    altarRecipe('bloodmagic:weakbloodorb', 'botania:mana_diamond', 0, 2000, 20, 2);
+    altarRecipe('bloodmagic:soulsnare', 'botania:mana_string', 0, 500, 10, 2);
+    altarRecipe('bloodmagic:daggerofsacrifice', 'botania:terra_sword', 1, 3000, 30, 6);
+    altarRecipe('bloodmagic:weak_tau', 'minecraft:sweet_berries', 1, 1500, 30, 6);
+    altarRecipe('bloodmagic:apprenticebloodorb', 'botania:elementium_ingot', 1, 5000, 30, 6);
+    altarRecipe('bloodmagic:magicianbloodorb', 'botania:terrasteel_ingot', 2, 25000, 100, 10);
+    altarRecipe('bloodmagic:airscribetool', 'botania:rune_air', 2, 1000, 100, 10);
+    altarRecipe('bloodmagic:waterscribetool', 'botania:rune_water', 2, 1000, 100, 10);
+    altarRecipe('bloodmagic:earthscribetool', 'botania:rune_earth', 2, 1000, 100, 10);
+    altarRecipe('bloodmagic:firescribetool', 'botania:rune_fire', 2, 1000, 100, 10);
+    altarRecipe('bloodmagic:duskscribetool', 'botania:rune_mana', 3, 2000, 200, 15);
+    altarRecipe('bloodmagic:masterbloodorb', 'mythicbotany:alfsteel_ingot', 3, 40000, 200, 15);
+})
